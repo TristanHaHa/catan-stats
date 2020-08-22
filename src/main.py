59cbc34454b7f9b4683f0991abf3961de1f8a118
Tristan-Ha <tristanha02@gmail.com>
2020-08-22 12:06:50 -0700

    added method to make rect from rect; made list of rects to track text boxes in playerMenu

--- src/main.py
+++ src/main.py
@@ -28,14 +28,6 @@ def mainMenu():
         pygame.display.update()
     playerMenu()
 
-def makeRect(x,y,w,h,outlineWidth,txt,color=(128,128,128)):
-    pygame.draw.rect(screen,(0,0,0),[x-w/2-outlineWidth,y-h/2-outlineWidth,w+outlineWidth*2,h+outlineWidth*2])
-    pygame.draw.rect(screen,color,[x-w/2,y-h/2,w,h])
-
-    font = pygame.font.SysFont(None, 40)
-    text = font.render(txt, True, (255,255,255))
-    screen.blit(text, [x-text.get_rect().width/2,y-text.get_rect().height/2,w,h])
-
 def playerMenu():
     screen.fill((191,25,25))
 
@@ -45,13 +37,37 @@ def playerMenu():
     screen.blit(titleTxt1, [width/2-325,height/2 - 300, 500, 100])
     screen.blit(titleTxt2, [width/2-250,height/2 - 250, 500, 100])
 
+    textBoxes = []
+    for i in range(1,5):
+        rect1 = pygame.Rect(250,225+ 75*i,140,40)
+        rect2 = pygame.Rect(450,225+ 75*i,140,40)
+        textBoxes.append(rect1)
+        textBoxes.append(rect2)
+
+        str = f"Player {i}"
+        makeRectFromRect(rect1,0,str,(255,255,255),(0,0,0))
+        makeRectFromRect(rect2,0,"Color",(255,255,255),(0,0,0))
+
     playerMenuRunning = True
     while playerMenuRunning:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            #if event.type == pygame.MOUSEBUTTONDOWN
+
         pygame.display.update()
+
+def makeRect(x,y,w,h,outlineWidth,txt="",bkgcolor=(128,128,128),txtcolor=(255,255,255)):
+    pygame.draw.rect(screen,(0,0,0),[x-w/2-outlineWidth,y-h/2-outlineWidth,w+outlineWidth*2,h+outlineWidth*2])
+    pygame.draw.rect(screen,bkgcolor,[x-w/2,y-h/2,w,h])
+
+    font = pygame.font.SysFont(None, 40)
+    text = font.render(txt, True, txtcolor)
+    screen.blit(text, [x-text.get_rect().width/2,y-text.get_rect().height/2,w,h])
+
+def makeRectFromRect(rectangle,outlineWidth,txt="",bkgcolor=(128,128,128),txtcolor=(255,255,255)):
+    makeRect(rectangle.left,rectangle.top,rectangle.w,rectangle.h,outlineWidth,txt,bkgcolor,txtcolor)
 
 def main():
     #mainMenu()
