a93d5e80760e94ec7fb69ebfb138891cc4542412
Tristan-Ha <tristanha02@gmail.com>
2020-08-21 23:21:22 -0700

    added main menu button functionality

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import pygame, random, sys
 
 def mainMenu():
+    global mainMenuRunning
+
     buttonX = width/2
     buttonY = height/2 + 50
     buttonWidth = 130
@@ -20,18 +22,26 @@ def mainMenu():
     titleTxt = titleFont.render('Catan Stats', True, (255,230,0))
     screen.blit(titleTxt, [width/2-250,height/2 - 150, 500, 100])
 
+    while mainMenuRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if buttonX-buttonWidth/2-outlineWidth <= pygame.mouse.get_pos()[0] <= (buttonX-buttonWidth/2-outlineWidth)+(buttonWidth+outlineWidth*2):
+                    if buttonY-buttonHeight/2-outlineWidth <= pygame.mouse.get_pos()[0] <= (buttonY-buttonHeight/2-outlineWidth)+(buttonHeight+outlineWidth*2):
+                        mainMenuRunning = False
+        pygame.display.update()
+    print("clicked button")
+
 def main():
     mainMenu()
-
-    while True:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
-        pygame.display.update()
 
 pygame.init()
 size = width, height = 700,700
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Catan Stats")
+
+mainMenuRunning = True
+
 main()
