bf7d9152276d79859e1f5b3f47beda2aaab4dfdb
Tristan-Ha <tristanha02@gmail.com>
2020-08-22 11:21:55 -0700

    repurposed makeButton function into makeRect function and created title text for playerMenu

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def mainMenu():
 
     screen.fill((191,25,25))
 
-    makeButton(buttonX,buttonY,buttonWidth,buttonHeight,outlineWidth,"Start")
+    makeRect(buttonX,buttonY,buttonWidth,buttonHeight,outlineWidth,"Start")
 
     titleFont = pygame.font.SysFont(None, 125)
     titleTxt = titleFont.render("Catan Stats", True, (255,230,0))
@@ -28,20 +28,22 @@ def mainMenu():
         pygame.display.update()
     playerMenu()
 
-def makeButton(x,y,w,h,outlineWidth,txt):
+def makeRect(x,y,w,h,outlineWidth,txt,color=(128,128,128)):
     pygame.draw.rect(screen,(0,0,0),[x-w/2-outlineWidth,y-h/2-outlineWidth,w+outlineWidth*2,h+outlineWidth*2])
-    pygame.draw.rect(screen,(128,128,128),[x-w/2,y-h/2,w,h])
+    pygame.draw.rect(screen,color,[x-w/2,y-h/2,w,h])
 
-    buttonFont = pygame.font.SysFont(None, 40)
-    startTxt = buttonFont.render(txt, True, (255,255,255))
-    screen.blit(startTxt, [x-startTxt.get_rect().width/2,y-startTxt.get_rect().height/2,w,h])
+    font = pygame.font.SysFont(None, 40)
+    text = font.render(txt, True, (255,255,255))
+    screen.blit(text, [x-text.get_rect().width/2,y-text.get_rect().height/2,w,h])
 
 def playerMenu():
     screen.fill((191,25,25))
 
-    titleFont = pygame.font.SysFont(None, 30)
-    titleTxt = titleFont.render("Please enter the players' names and colors in turn order", True, (255,230,0))
-    screen.blit(titleTxt, [width/2-250,height/2 - 150, 500, 100])
+    titleFont = pygame.font.SysFont(None, 60)
+    titleTxt1 = titleFont.render("Please enter the players' names", True, (255,230,0))
+    titleTxt2 = titleFont.render("and colors in turn order", True, (255,230,0))
+    screen.blit(titleTxt1, [width/2-325,height/2 - 300, 500, 100])
+    screen.blit(titleTxt2, [width/2-250,height/2 - 250, 500, 100])
 
     playerMenuRunning = True
     while playerMenuRunning:
@@ -52,7 +54,8 @@ def playerMenu():
         pygame.display.update()
 
 def main():
-    mainMenu()
+    #mainMenu()
+    playerMenu()
 
 pygame.init()
 size = width, height = 700,700
