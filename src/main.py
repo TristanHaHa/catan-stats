3a64dac795a3a8466c1e7f1efd212a92d426ed3d
Tristan-Ha <tristanha02@gmail.com>
2020-08-22 10:26:50 -0700

    further expanded main menu button functionality and created player menu screen

--- src/main.py
+++ src/main.py
@@ -23,10 +23,10 @@ def mainMenu():
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if buttonX-buttonWidth/2-outlineWidth <= pygame.mouse.get_pos()[0] <= (buttonX-buttonWidth/2-outlineWidth)+(buttonWidth+outlineWidth*2):
-                    if buttonY-buttonHeight/2-outlineWidth <= pygame.mouse.get_pos()[0] <= (buttonY-buttonHeight/2-outlineWidth)+(buttonHeight+outlineWidth*2):
+                    if buttonY-buttonHeight/2-outlineWidth <= pygame.mouse.get_pos()[1] <= (buttonY-buttonHeight/2-outlineWidth)+(buttonHeight+outlineWidth*2):
                         mainMenuRunning = False
         pygame.display.update()
-    print("clicked button")
+    playerMenu()
 
 def makeButton(x,y,w,h,outlineWidth,txt):
     pygame.draw.rect(screen,(0,0,0),[x-w/2-outlineWidth,y-h/2-outlineWidth,w+outlineWidth*2,h+outlineWidth*2])
@@ -35,6 +35,21 @@ def makeButton(x,y,w,h,outlineWidth,txt):
     buttonFont = pygame.font.SysFont(None, 40)
     startTxt = buttonFont.render(txt, True, (255,255,255))
     screen.blit(startTxt, [x-startTxt.get_rect().width/2,y-startTxt.get_rect().height/2,w,h])
+
+def playerMenu():
+    screen.fill((191,25,25))
+
+    titleFont = pygame.font.SysFont(None, 30)
+    titleTxt = titleFont.render("Please enter the players' names and colors in turn order", True, (255,230,0))
+    screen.blit(titleTxt, [width/2-250,height/2 - 150, 500, 100])
+
+    playerMenuRunning = True
+    while playerMenuRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        pygame.display.update()
 
 def main():
     mainMenu()
